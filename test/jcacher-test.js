b2d6756b75f3c619b90f6e7036f99e41a0f7786a
Andreas Brantmo <andreas@deckhand.se>
2011-12-19 10:42:42 +0100

    Remove test cases for 'count'

--- test/jcacher-test.js
+++ test/jcacher-test.js
@@ -1,30 +1,6 @@
 jCacher.CacheItem.defaults.sliding = 120;
 jCacher.CacheItem.defaults.absolute = null;
 
-test('and, remove and count', function () {
-
-	var myKey1='myKey1',
-		myValue1='myValue1',
-		myKey2 = 'myKey2',
-		myValue2 = 'myValue2';
-	
-	jCacher.add(myKey1, myValue1);
-	ok(jCacher.count === 1, 'Count is 1')
-	
-	jCacher.add(myKey1, myValue1);
-	ok(jCacher.count === 1, 'Count is still 1')
-	
-	jCacher.add(myKey2, myValue2);
-	ok(jCacher.count === 2, 'Count is now 2');
-	
-	var success = jCacher.remove(myKey1);
-	ok(jCacher.count === 1, 'Count is back on 1');
-	
-	var itemsRemoved = jCacher.reset();
-	ok(jCacher.count === 0, 'Count is back on 0');
-	
-});
-
 test('add remove and get', function(){
 	
 	var myKey1='myKey1',
